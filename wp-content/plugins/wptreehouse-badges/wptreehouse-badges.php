6d134dd1aceb0036fd6ff19d41cba0980a9ab1fd
7Mears <7mears@gmail.com>
2014-10-20 18:01:12 -0600

    treehouse badges pre AJAX

--- wp-content/plugins/wptreehouse-badges/wptreehouse-badges.php
+++ wp-content/plugins/wptreehouse-badges/wptreehouse-badges.php
@@ -103,6 +103,9 @@ class Wptreehouse_Badges_Widget extends WP_Widget {
 
 		extract( $args );
 		$title = apply_filters( 'widget_title', $instance['title'] );
+		$num_badges = $instance['num_badges'];
+		$display_tooltip = $instance['display_tooltip'];
+
 
 		$options = get_option( 'wptreehouse_badges' );
 		$wptreehouse_profile = $options['wptreehouse_profile'];
@@ -116,6 +119,8 @@ class Wptreehouse_Badges_Widget extends WP_Widget {
 
 		$instance = $old_instance;
 		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['num_badges'] = strip_tags($new_instance['num_badges']);
+		$instance['display_tooltip'] = strip_tags($new_instance['display_tooltip']);
 
 		return $instance;
 
@@ -125,6 +130,11 @@ class Wptreehouse_Badges_Widget extends WP_Widget {
 		// Output admin widget options form
 
 		$title = esc_attr( $instance['title'] );
+		$num_badges = esc_attr($instance['num_badges']);
+		$display_tooltip = esc_attr($instance['display_tooltip']);
+
+		$options = get_option( 'wptreehouse_badges' );
+		$wptreehouse_profile = $options['wptreehouse_profile'];
 
 		require( 'inc/widget-fields.php' );
 	}
@@ -136,6 +146,35 @@ function wp_treehouse_badges_register_widgets() {
 
 add_action( 'widgets_init', 'wp_treehouse_badges_register_widgets' );
 
+function wptreehouse_badges_shortcode( $atts, $content = null ) {
+
+	global $post;
+
+	extract( shortcode_atts( array(
+		'num_badges'	=> '8',
+		'tooltip' 		=> 'on'
+
+	), $atts) );
+
+	if( $tooltip == 'on' ) $tooltip = 1;
+	if( $tooltip == 'off' ) $tooltip = 0;
+
+	$display_tooltip = $tooltip;
+
+	$options = get_option( 'wptreehouse_badges' );
+	$wptreehouse_profile = $options['wptreehouse_profile'];
+
+	ob_start();
+
+	require( 'inc/front-end.php' );
+
+	$content = ob_get_clean();
+
+	return $content;
+
+}
+
+add_shortcode( 'wptreehouse_badges', 'wptreehouse_badges_shortcode');
 
 function wptreehouse_badges_get_profile( $wptreehouse_username ) {
 
@@ -150,11 +189,28 @@ function wptreehouse_badges_get_profile( $wptreehouse_username ) {
 
 }
 
-function wptreehouse_badges_styles() {
+function wptreehouse_badges_refresh_profiles() {
 
-	wp_enqueue_style( 'wptreehouse_badges_styles', plugins_url( 'wptreehouse-badges/wptreehouse-badges.css' ) );
+	$options = get_option( 'wptreehouse_badges' );
+	$last_updated = $options['last_updated'];
+
+	$current_time = time();
 
 }
-add_action ( 'admin_head', 'wptreehouse_badges_styles' );
+
+function wptreehouse_badges_backend_styles() {
+
+	wp_enqueue_style( 'wptreehouse_badges_backend_css', plugins_url( 'wptreehouse-badges/wptreehouse-badges.css' ) );
+
+}
+add_action ( 'admin_head', 'wptreehouse_badges_backend_styles' );
+
+function wptreehouse_badges_frontend_scripts_and_styles() {
+
+	wp_enqueue_style( 'wptreehouse_badges_frontend_css', plugins_url( 'wptreehouse-badges/wptreehouse-badges.css' ) );
+	wp_enqueue_script( 'wptreehouse_badges_frontend_js', plugins_url( 'wptreehouse-badges/wptreehouse-badges.js' ), array('jquery'), '', true );
+
+}
+add_action ( 'wp_enqueue_scripts', 'wptreehouse_badges_frontend_scripts_and_styles' );
 
 ?>
