4f2ce6e0793a09fce8fa8240f6c1bf0ce46f7f58
Jordan Mears <7mears@gmail.com>
2014-11-03 22:28:43 -0600

    **Version 1.0**

--- wp-content/themes/mears/front-page.php
+++ wp-content/themes/mears/front-page.php
@@ -6,7 +6,7 @@
  * @package Mears
  * @subpackage Customizations
  */
- 
+
 add_action( 'wp_enqueue_scripts', 'mears_enqueue_home_scripts' );
 /**
  * Enqueue Scripts
@@ -16,7 +16,7 @@ function mears_enqueue_home_scripts() {
 	wp_enqueue_script( 'mears-home', get_bloginfo( 'stylesheet_directory' ) . '/js/home.js', array( 'jquery' ), '1.0.0', true );
 	wp_enqueue_script( 'localScroll', get_stylesheet_directory_uri() . '/js/jquery.localScroll.min.js', array( 'scrollTo' ), '1.2.8b', true );
 	wp_enqueue_script( 'scrollTo', get_stylesheet_directory_uri() . '/js/jquery.scrollTo.min.js', array( 'jquery' ), '1.4.5-beta', true );
- 	
+
 }
 
 add_action( 'genesis_meta', 'mears_home_genesis_meta' );
@@ -26,61 +26,33 @@ add_action( 'genesis_meta', 'mears_home_genesis_meta' );
  */
 function mears_home_genesis_meta() {
 
-	if ( is_active_sidebar( 'hero-section' ) || is_active_sidebar( 'about-section' ) || is_active_sidebar( 'portfolio-section' ) ) {
+	if ( is_active_sidebar( 'hero-section' ) || is_active_sidebar( 'portfolio-section' ) ) {
 
 		//* Force full width content layout
 		add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
 
-		//* Add mears-pro-home body class
-		add_filter( 'body_class', 'mears_body_class' );
-		
 		//* Remove breadcrumbs
 		remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
-
 		//* Remove the default Genesis loop
 		remove_action( 'genesis_loop', 'genesis_do_loop' );
-		
-		//* Add home featured widget
-		add_action( 'genesis_after_header', 'mears_home_featured_widget', 1 );
-		
+
 		//* Add home widgets
-		add_action( 'genesis_before_footer', 'mears_home_widgets', 5 );
-		
+		add_action( 'genesis_before_footer', 'mears_home_widgets', 2 );
+
 	}
-}
-
-function mears_body_class( $classes ) {
-
-	$classes[] = 'mears-pro-home';
-	return $classes;
-	
-}
-
-function mears_home_featured_widget() {
-
-	genesis_widget_area( 'hero-section', array(
-		'before' => '<div id="hero" class="hero-section"><div class="wrap">',
-		'after'  => '</div></div>',
-	) );
-	
 }
 
 function mears_home_widgets() {
 
-	echo '<div id="home-widgets" class="home-widgets">';
-	
+genesis_widget_area( 'hero-section', array(
+  'before' => '<div id="hero" class="wrap">',
+  'after'  => '</div>',
+) );
+
 	genesis_widget_area( 'portfolio-section', array(
-		'before' => '<div id="portfolio" class="portfolio-section wrap">',
+		'before' => '<div id="portfolio" class="wrap">',
 		'after'  => '</div>',
 	) );
-	
-	genesis_widget_area( 'about-section', array(
-		'before' => '<div id="about" class="about-section wrap">',
-		'after'  => '</div>',
-	) );
-
-	echo '</div>';
-
 }
 
 genesis();
--- wp-content/themes/mears/functions.php
+++ wp-content/themes/mears/functions.php
@@ -11,17 +11,7 @@ define( 'CHILD_THEME_VERSION', '1.0' );
 add_action( 'wp_enqueue_scripts', 'mears_google_fonts' );
 function mears_google_fonts() {
 
-	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Montserrat|Open+Sans:700,300', array(), 1.0 );
-}
-
-
-//* Enqueue and Load Font Awesome
-
-add_action( 'wp_enqueue_scripts', 'afn_enqueue_awesome' );
-function afn_enqueue_awesome() {
-	if ( !is_admin() ) {
-		wp_enqueue_style( 'afn-font-awesome', get_bloginfo( 'stylesheet_directory' ) . '/css/font-awesome.min.css', array(), '4.0.3' );
-	}
+	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Montserrat|Open+Sans:700,400', array(), 1.0 );
 }
 
 //* Add HTML5 markup structure
@@ -29,16 +19,6 @@ add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list'
 
 //* Add new image sizes
 add_image_size( 'portfolio-section', 800, '800', TRUE );
-
-//* Add viewport meta tag for mobile browsers
-add_theme_support( 'genesis-responsive-viewport' );
-
-//* Add support for custom background
-add_theme_support( 'custom-background' );
-
-//* Add support for 3-column footer widgets
-add_theme_support( 'genesis-footer-widgets', 3 );
-
 
 //* Register widget areas
 genesis_register_sidebar( array(
@@ -51,11 +31,6 @@ genesis_register_sidebar( array(
 	'name'        => __( 'Portfolio Section', 'mears' ),
 	'description' => __( 'This is the fourth section of the home page.', 'mears' ),
 ) );
-genesis_register_sidebar( array(
-	'id'          => 'about-section',
-	'name'        => __( 'About Section', 'mears' ),
-	'description' => __( 'This is the second section of the home page.', 'mears' ),
-) );
 
 //* Customize the post info function
 add_filter( 'genesis_post_info', 'sp_post_info_filter' );
@@ -65,13 +40,36 @@ if ( !is_page() ) {
 	return $post_info;
 }}
 
+//* Remove support for 3-column footer widgets
+remove_theme_support( 'genesis-footer-widgets', 1 );
+//* Remove the entry meta in the entry footer (requires HTML5 theme support)
+remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
+/** Remove default sidebar */
+remove_action( 'genesis_sidebar', 'genesis_do_sidebar' );
+//* Unregister secondary sidebar
+unregister_sidebar( 'sidebar-alt' );
+//* Unregister layout settings
+genesis_unregister_layout( 'content-sidebar' );
+genesis_unregister_layout( 'sidebar-content' );
+genesis_unregister_layout( 'content-sidebar-sidebar' );
+genesis_unregister_layout( 'sidebar-content-sidebar' );
+genesis_unregister_layout( 'sidebar-sidebar-content' );
 
-//* Removes comments in the galleries
-function filter_media_comment_status( $open, $post_id ) {
-    $post = get_post( $post_id );
-    if( $post->post_type == 'attachment' ) {
-        return false;
-    }
-    return $open;
+
+//* Removes some in-line styling
+add_action( 'widgets_init', 'my_remove_recent_comments_style' );
+function my_remove_recent_comments_style() {
+	global $wp_widget_factory;
+	remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style'  ) );
 }
-add_filter( 'comments_open', 'filter_media_comment_status', 10 , 2 );
+
+
+//* Customize the credits
+add_filter( 'genesis_footer_creds_text', 'sp_footer_creds_text' );
+function sp_footer_creds_text() {
+	echo '<div class="creds"><p>';
+	echo 'Copyright &copy; ';
+	echo date('Y');
+	echo ' &middot; <a href="http://www.jordanmears.com">Jordan Mears</a> ';
+	echo '</p></div>';
+}
